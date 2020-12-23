fdeb1b67d6d26435bf6fb2b24d83a831b11ce9d0
GangbacOl <gangyeonjo336@gmail.com>
2020-12-23 23:32:37 +0900

    UPDATE action creator in store/actions/formAction.js

--- src/store/actions/formAction.js
+++ src/store/actions/formAction.js
@@ -1,15 +1,21 @@
 import { createAction } from 'redux-actions';
 
+export const UPLOAD_ARTICLE_LOADING = 'article/UPLOAD_ARTICLE_LOADING';
+export const UPLOAD_ARTICLE_SUCCESS = 'article/UPLOAD_ARTICLE_SUCCESS';
+export const UPLOAD_ARTICLE_ERROR = 'article/UPLOAD_ARTICLE_ERROR';
+
 export const TOGGLE_CATEGORY = 'form/TOGGLE_CATEGORY';
 export const ADD_PARTICIPANT = 'form/ADD_PARTICIPANT';
 export const ADD_CLUB = 'form/ADD_CLUB';
 export const ADD_KIND = 'form/ADD_KIND';
+export const ADD_IMAGE_FILE = 'form/ADD_IMAGE_FILE';
+export const ADD_IMAGE_NAME = 'form/ADD_IMAGE_NAME';
 export const CHANGE_CONTENT = 'form/CHANGE_CONTENT';
-export const ADD_IMAGE = 'form/ADD_IMAGE';
 
+export const uploadArticleAsync = createAction(UPLOAD_ARTICLE_LOADING, (input) => input);
 export const toggleCategory = createAction(TOGGLE_CATEGORY, (input) => input);
 export const addParticipant = createAction(ADD_PARTICIPANT, (input) => input);
 export const addClub = createAction(ADD_CLUB, (input) => input);
 export const addKind = createAction(ADD_KIND, (input) => input);
+export const addImageFile = createAction(ADD_IMAGE_FILE, (input) => input);
 export const changeContent = createAction(CHANGE_CONTENT, (input) => input);
-export const addImage = createAction(ADD_IMAGE, (input) => input);
