e0eff2826abc00c8943400199e9d374f10657ab0
GangbacOl <gangyeonjo336@gmail.com>
2020-12-24 04:35:21 +0900

    UPDATE props, child components in components/Form/Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,44 +3,53 @@ import React from 'react';
 import InputForm from './InputForm';
 import PreviewForm from './PreviewForm';
 import ButtonForm from './ButtonForm';
+import ImageForm from './ImageForm';
 
 import './Form.scss';
 
 const Form = ({
-    category,
+    isContestWork,
     participants,
     clubs,
     kinds,
     content,
-    uploadedImages,
-    toggleCategory,
+    imageFiles,
+    uploadArticleAsync,
+    toggleIsContestWork,
     addParticipant,
     addClub,
     addKind,
+    addImageFile,
     changeContent,
-    addImage,
 }) => {
     return (
         <div className="Form">
-            <InputForm
-                content={content}
-                uploadedImages={uploadedImages}
-                toggleCategory={toggleCategory}
-                addParticipant={addParticipant}
-                addClub={addClub}
-                addKind={addKind}
-                changeContent={changeContent}
-                addImage={addImage}
-            />
-            <PreviewForm participants={participants} clubs={clubs} kinds={kinds} />
-            <ButtonForm
-                category={category}
-                participants={participants}
-                clubs={clubs}
-                kinds={kinds}
-                content={content}
-                uploadedImages={uploadedImages}
-            />
+            <h2>정보보호과 홍보 페이지</h2>
+            <p>게시글 작성 페이지</p>
+            <div>
+                <div className="Form-left">
+                    <InputForm
+                        toggleIsContestWork={toggleIsContestWork}
+                        addParticipant={addParticipant}
+                        addClub={addClub}
+                        addKind={addKind}
+                        changeContent={changeContent}
+                    />
+                    <ButtonForm
+                        isContestWork={isContestWork}
+                        participants={participants}
+                        clubs={clubs}
+                        kinds={kinds}
+                        content={content}
+                        imageFiles={imageFiles}
+                        uploadArticleAsync={uploadArticleAsync}
+                    />
+                </div>
+                <div className="Form-right">
+                    <PreviewForm participants={participants} clubs={clubs} kinds={kinds} />
+                    <ImageForm addImageFile={addImageFile} />
+                </div>
+            </div>
         </div>
     );
 };
