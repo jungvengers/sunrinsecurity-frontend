9cd628f0d35537204ae92b47b189fc777586890b
GangbacOl <gangyeonjo336@gmail.com>
2020-12-24 04:28:12 +0900

    UPDATE import path in index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import rootReducer, { rootSaga } from './modules';
+import rootReducer, { rootSaga } from './store';
 
 import App from './App';
 
