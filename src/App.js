c47b39f6728dd30a14de063a20dd7226faa5b5b3
GangbacOl <gangyeonjo336@gmail.com>
2020-12-27 22:29:47 +0900

    ADD utils/lib/history.js for using history object

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Router } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
 
 import FormContainer from './containers/FormContainer';
 import MainContainer from './containers/MainContainer';
 import LoginContainer from './containers/LoginContainer';
 import RegisterContainer from './containers/RegisterContainer';
 
+import history from './utils/lib/history';
+
 import './App.css';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <Switch>
                 <Route exact path="/" component={MainContainer} />
                 <Route path="/login" component={LoginContainer} />
                 <Route path="/register" component={RegisterContainer} />
                 <Route path="/admin/form" component={FormContainer} />
             </Switch>
-        </BrowserRouter>
+        </Router>
     );
 };
 
