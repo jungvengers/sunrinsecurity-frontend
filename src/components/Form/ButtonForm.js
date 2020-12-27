395399d70bcb3f38b5d8fbfbf34f2e424cdcbbb1
GangbacOl <gangyeonjo336@gmail.com>
2020-12-27 23:02:44 +0900

    UPDATE to add history.push, antd in components/Form/ButtonForm.js

--- src/components/Form/ButtonForm.js
+++ src/components/Form/ButtonForm.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { useHistory } from 'react-router';
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
 
 import { formValidation } from '../../utils/lib/validationCheck';
+import history from '../../utils/lib/history';
 
 import './ButtonForm.scss';
 
@@ -14,7 +15,9 @@ const ButtonForm = ({
     imageFiles,
     uploadArticleAsync,
 }) => {
-    const history = useHistory();
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handleClose = () => setIsModalVisible(false);
 
     const handleClick = async () => {
         const data = {
@@ -25,22 +28,27 @@ const ButtonForm = ({
             kinds,
             imageFiles,
         };
-
         if (formValidation(data)) {
             uploadArticleAsync(data);
         } else {
-            alert('모든 양식을 채워주세요.');
+            Modal.warning({
+                title: '모든 양식을 채워주세요.',
+                //TODO 어떤 영역 안채웠는지 메시징
+                content: '모든 양식을 채워주세요.',
+            });
         }
     };
     return (
         <div className="ButtonForm">
-            <input
-                type="button"
-                className="ButtonForm-cancel"
-                value="취소"
-                onClick={() => history.push('/')}
-            />
-            <input type="button" className="ButtonForm-save" value="저장" onClick={handleClick} />
+            <Modal title="양식 미작성" visible={isModalVisible} onOk={handleClose} onCancel={handleClose}>
+                <p>모든 양식을 채워주세요.</p>
+            </Modal>
+            <Button type="primary" onClick={handleClick}>
+                저장
+            </Button>
+            <Button type="primary" onClick={() => history.push('/')} danger>
+                취소
+            </Button>
         </div>
     );
 };
