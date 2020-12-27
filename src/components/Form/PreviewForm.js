c84c1428dad153ef31f53c277e92d8599b0dcbb7
GangbacOl <gangyeonjo336@gmail.com>
2020-12-27 22:52:44 +0900

    UPDATE key prop in map method in components/Form/PreviewForm.js

--- src/components/Form/PreviewForm.js
+++ src/components/Form/PreviewForm.js
@@ -8,24 +8,24 @@ const PreviewForm = ({ participants, clubs, kinds }) => {
             <div className="PreviewForm-section">
                 <p>참가자</p>
                 <div className="PreviewForm-preview-data">
-                    {participants.map((item) => (
-                        <span>{item}</span>
+                    {participants.map((item, key) => (
+                        <span key={key}>{item}</span>
                     ))}
                 </div>
             </div>
             <div className="PreviewForm-section">
                 <p>소속</p>
                 <div className="PreviewForm-preview-data">
-                    {clubs.map((item) => (
-                        <span>{item}</span>
+                    {clubs.map((item, key) => (
+                        <span key={key}>{item}</span>
                     ))}
                 </div>
             </div>
             <div className="PreviewForm-section">
                 <p>분야</p>
                 <div className="PreviewForm-preview-data">
-                    {kinds.map((item) => (
-                        <span>{item}</span>
+                    {kinds.map((item, key) => (
+                        <span key={key}>{item}</span>
                     ))}
                 </div>
             </div>
