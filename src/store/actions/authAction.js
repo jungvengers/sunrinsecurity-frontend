af0f0183baa7e4175fba7c7df0bb2c402f48c326
GangbacOl <gangyeonjo336@gmail.com>
2020-12-30 05:51:22 +0900

    UPDATE to add username state in store/

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,12 +1,14 @@
 import { createAction } from 'redux-actions';
 
-export const LOGIN_LOADING = 'LOGIN_LOADING';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const INIT_USERNAME = 'auth/INIT_USERNAME';
 
-export const REGISTER_LOADING = 'REGISTER_LOADING';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_ERROR = 'REGISTER_ERROR';
+export const LOGIN_LOADING = 'auth/LOGIN_LOADING';
+export const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+export const LOGIN_ERROR = 'auth/LOGIN_ERROR';
+
+export const REGISTER_LOADING = 'auth/REGISTER_LOADING';
+export const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+export const REGISTER_ERROR = 'auth/REGISTER_ERROR';
 
 export const loginAsync = createAction(LOGIN_LOADING, (input) => input);
 export const registerAsync = createAction(REGISTER_LOADING, (input) => input);
