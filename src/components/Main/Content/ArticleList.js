316a46e6d4041126f7d5b9a645a09912c4704292
GangbacOl <gangyeonjo336@gmail.com>
2021-01-11 22:19:43 +0900

    ADD ArticleList.js for wrapping ArticleItem components

---
+++ src/components/Main/Content/ArticleList.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Card } from 'antd';
+
+import ArticleItem from './ArticleItem';
+
+import './ArticleList.scss';
+
+const ArticleList = ({ articles, category, clubs, kinds, readArticle, readMoreArticle, deleteArticle }) => {
+    const [articlePerPage, setArticlePerPage] = useState(5);
+    const [currentPage, setCurrentPage] = useState(1);
+    useEffect(() => {
+        const queryStringData = {
+            articlePerPage,
+            currentPage: 1,
+            category,
+            clubs,
+            kinds,
+        };
+        readArticle(queryStringData);
+        setCurrentPage(2);
+    }, [category, clubs, kinds]);
+
+    const getArticles = () => {
+        const queryStringData = {
+            articlePerPage,
+            currentPage,
+            category,
+            clubs,
+            kinds,
+        };
+        readMoreArticle(queryStringData);
+        setCurrentPage(currentPage + 1);
+    };
+
+    return (
+        <div className="ArticleList">
+            <InfiniteScroll
+                className="infiniteScrollWrapper"
+                dataLength={articles.length}
+                next={getArticles}
+                hasMore={true}>
+                {articles
+                    ? articles.map((article) => {
+                          return (
+                              <Card
+                                  title={article.isContestWork ? '대회실적' : '개인 프로젝트'}
+                                  style={{ width: '100%', marginTop: '15px' }}
+                                  key={article._id}>
+                                  <ArticleItem
+                                      clubs={article.clubs}
+                                      content={article.content}
+                                      images={article.images}
+                                      kinds={article.kinds}
+                                      participants={article.participants}
+                                      id={article._id}
+                                      deleteArticle={deleteArticle}
+                                  />
+                              </Card>
+                          );
+                      })
+                    : '해당 게시물을 찾을 수 없습니다.'}
+            </InfiniteScroll>
+        </div>
+    );
+};
+
+export default ArticleList;
