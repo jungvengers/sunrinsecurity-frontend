56585a794541bbda8c58a476c5d2252ea2c62af8
GangbacOl <gangyeonjo336@gmail.com>
2021-01-15 01:37:17 +0900

    UPDATE props, validation logic in components/Form/Submit.js

--- src/components/Form/Submit.js
+++ src/components/Form/Submit.js
@@ -2,31 +2,47 @@ import React, { useState } from 'react';
 import { Modal, Button } from 'antd';
 
 import { formValidation } from '../../utils/lib/validationCheck';
+import { splitParticipants, splitYoutubeURLs } from '../../utils/lib/splitString';
 import history from '../../utils/lib/history';
 
 import './Submit.scss';
 
-const Submit = ({ isContestWork, participants, clubs, kinds, content, imageFiles, uploadArticleAsync }) => {
+const Submit = ({
+    isContestWork,
+    participants,
+    clubs,
+    kinds,
+    content,
+    imageFiles,
+    youtubeURLs,
+    uploadArticleAsync,
+}) => {
     const [isModalVisible, setIsModalVisible] = useState(false);
 
     const handleClose = () => setIsModalVisible(false);
-
+    const handleCancel = () => history.push('/');
     const handleClick = async () => {
         const data = {
-            isContestWork,
-            participants,
+            participants: splitParticipants(participants),
             clubs,
-            content,
             kinds,
+            isContestWork,
             imageFiles,
+            youtubeURLs: splitYoutubeURLs(youtubeURLs),
+            content,
         };
-        if (formValidation(data)) {
+        const [result, missingFieldName] = formValidation(data, [
+            'participants',
+            'kinds',
+            'isContestWork',
+            'content',
+        ]);
+        if (result) {
             uploadArticleAsync(data);
         } else {
             Modal.warning({
                 title: '모든 양식을 채워주세요.',
-                //TODO 어떤 영역 안채웠는지 메시징
-                content: '모든 양식을 채워주세요.',
+                content: `${missingFieldName} 칸을(를) 채워주세요.`,
             });
         }
     };
@@ -36,7 +52,7 @@ const Submit = ({ isContestWork, participants, clubs, kinds, content, imageFiles
             <Modal title="양식 미작성" visible={isModalVisible} onOk={handleClose} onCancel={handleClose}>
                 <p>모든 양식을 채워주세요.</p>
             </Modal>
-            <Button type="primary" onClick={() => history.push('/')} danger>
+            <Button type="primary" onClick={handleCancel} danger>
                 취소
             </Button>
             <Button type="primary" onClick={handleClick}>
@@ -46,4 +62,4 @@ const Submit = ({ isContestWork, participants, clubs, kinds, content, imageFiles
     );
 };
 
-export default Submit;
+export default React.memo(Submit);
