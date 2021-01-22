83dcb485f8c03596ec91969438dbc345f55d345d
GangbacOl <gangyeonjo336@gmail.com>
2021-01-23 01:56:03 +0900

    UPDATE container name, route logic in src/App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Router } from 'react-router';
 import { Route, Switch } from 'react-router-dom';
 
-import FormContainer from './containers/FormContainer';
+import ManageContainer from './containers/ManageContainer';
+import EditFormContainer from './containers/EditFormContainer';
+import UploadFormContainer from './containers/UploadFormContainer';
 import MainContainer from './containers/MainContainer';
 import LoginContainer from './containers/LoginContainer';
 import RegisterContainer from './containers/RegisterContainer';
@@ -19,7 +21,9 @@ const App = () => {
                 <Route exact path="/" component={MainContainer} />
                 <Route path="/login" component={LoginContainer} />
                 <Route path="/register" component={RegisterContainer} />
-                <Route path="/admin/form" component={FormContainer} />
+                <Route path="/admin/form" component={UploadFormContainer} />
+                <Route exact path="/admin/edit" component={ManageContainer} />
+                <Route path="/admin/edit/:id" component={EditFormContainer} />
             </Switch>
         </Router>
     );
