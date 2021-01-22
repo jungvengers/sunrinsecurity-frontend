470dd9a95f151c261212df8b49662d03100d2f0d
GangbacOl <gangyeonjo336@gmail.com>
2021-01-23 04:19:13 +0900

    UPDATE TO ADD readAnArticle function, http method update to patch

--- src/utils/api/article.js
+++ src/utils/api/article.js
@@ -9,11 +9,12 @@ const headers = {
     },
 };
 
-// res -> response
-export const readArticle = async (data) => {
+export const readListArticles = async (data) => {
     const queryString = createQueryString(data);
     const response = await axios.get(`${ARTICLE_API_URL}?${queryString}`);
     return response.data.articles;
 };
-export const updateArticle = async (data) => await axios.update(`${ARTICLE_API_URL}/${data}`, data, headers);
+export const readAnArticle = async (data) => await axios.get(`${ARTICLE_API_URL}/${data}`);
+export const updateArticle = async (data) =>
+    await axios.patch(`${ARTICLE_API_URL}/${data.id}`, data.updateData, headers);
 export const deleteArticle = async (data) => await axios.delete(`${ARTICLE_API_URL}/${data}`, headers);
