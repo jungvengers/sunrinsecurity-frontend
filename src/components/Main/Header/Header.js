917a665345ecfd5fd57b61433f277096a8fce53b
GangbacOl <gangyeonjo336@gmail.com>
2021-01-23 16:15:56 +0900

    UPDATE Header item in src/components/Main/Header/Header.js

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -4,6 +4,14 @@ import { Layout, Menu } from 'antd';
 
 import './Header.scss';
 
+import {
+    SearchOutlined,
+    CommentOutlined,
+    ProjectOutlined,
+    SoundOutlined,
+    PaperClipOutlined,
+} from '@ant-design/icons';
+
 const Header = () => {
     const handleLogout = () => {
         localStorage.removeItem('accessToken');
@@ -15,22 +23,54 @@ const Header = () => {
             <Menu theme="dark" mode="horizontal" style={{ float: 'right' }}>
                 {localStorage.getItem('accessToken') ? (
                     <>
-                        <Menu.Item key="1">
-                            <Link to="/admin/form">글 작성</Link>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Link to="/admin/edit">글 수정</Link>
-                        </Menu.Item>
-                        <Menu.Item key="3" onClick={handleLogout}>
+                        <Menu.SubMenu key="1" title="학과">
+                            <Menu.Item key="1" icon={<SearchOutlined />}>
+                                <Link to="/">학과 소개</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<CommentOutlined />}>
+                                <Link to="/">학과 소식</Link>
+                            </Menu.Item>
+                            <Menu.Item key="3" icon={<ProjectOutlined />}>
+                                <Link to="/">프로젝트 전시장</Link>
+                            </Menu.Item>
+                        </Menu.SubMenu>
+                        <Menu.SubMenu key="2" title="글 작성">
+                            <Menu.Item key="1" icon={<SoundOutlined />}>
+                                <Link to="/admin/form">홍보글</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<PaperClipOutlined />}>
+                                <Link to="/admin/form">게시글</Link>
+                            </Menu.Item>
+                        </Menu.SubMenu>
+                        <Menu.SubMenu key="3" title="글 관리">
+                            <Menu.Item key="1" icon={<SoundOutlined />}>
+                                <Link to="/admin/edit">홍보글</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<PaperClipOutlined />}>
+                                <Link to="/admin/edit">게시글</Link>
+                            </Menu.Item>
+                        </Menu.SubMenu>
+                        <Menu.Item key="4" onClick={handleLogout}>
                             로그아웃
                         </Menu.Item>
                     </>
                 ) : (
                     <>
-                        <Menu.Item key="1">
+                        <Menu.SubMenu key="1" title="학과">
+                            <Menu.Item key="1" icon={<SearchOutlined />}>
+                                <Link to="/">학과 소개</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<CommentOutlined />}>
+                                <Link to="/">학과 소식</Link>
+                            </Menu.Item>
+                            <Menu.Item key="3" icon={<ProjectOutlined />}>
+                                <Link to="/">프로젝트 전시장</Link>
+                            </Menu.Item>
+                        </Menu.SubMenu>
+                        <Menu.Item key="2">
                             <Link to="/login">관리자 로그인</Link>
                         </Menu.Item>
-                        <Menu.Item key="2">
+                        <Menu.Item key="3">
                             <Link to="/register">관리자 회원가입</Link>
                         </Menu.Item>
                     </>
