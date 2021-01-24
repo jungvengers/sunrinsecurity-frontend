8e402c0eb9fc422b5bf7882213560b847ebb21c6
GangbacOl <gangyeonjo336@gmail.com>
2021-01-24 17:54:06 +0900

    UPDATE TO ADD reducer, saga in src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,35 @@
 import { combineReducers } from 'redux';
 import upload from './reducers/uploadReducer';
-import manage from './reducers/manageReducer';
+import manageArticle from './reducers/manageArticleReducer';
+import manageNotice from './reducers/manageNoticeReducer';
 import article from './reducers/articleReducer';
+import notice from './reducers/noticeReducer';
 import auth from './reducers/authReducer';
 import { uploadSaga } from './sagas/uploadSaga';
-import { manageSaga } from './sagas/manageSaga';
+import { manageArticleSaga } from './sagas/manageArticleSaga';
+import { manageNoticeSaga } from './sagas/manageNoticeSaga';
 import { articleSaga } from './sagas/articleSaga';
+import { noticeSaga } from './sagas/noticeSaga';
 import { authSaga } from './sagas/authSaga';
 import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({
     upload,
-    manage,
+    manageArticle,
+    manageNotice,
     article,
+    notice,
     auth,
 });
 export function* rootSaga() {
-    yield all([uploadSaga(), manageSaga(), articleSaga(), authSaga()]);
+    yield all([
+        uploadSaga(),
+        manageArticleSaga(),
+        manageNoticeSaga(),
+        articleSaga(),
+        noticeSaga(),
+        authSaga(),
+    ]);
 }
 
 export default rootReducer;
