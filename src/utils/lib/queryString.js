3c1c0ec2550858f911806c1c3a75eef7ea425c61
GangbacOl <gangyeonjo336@gmail.com>
2021-01-24 17:27:47 +0900

    UPDATE property name, try catch logic in src/utils/lib/queryString.js

--- src/utils/lib/queryString.js
+++ src/utils/lib/queryString.js
@@ -1,14 +1,17 @@
 export const createQueryString = (data) => {
-    let queryString = ``;
-
-    if (data.articlePerPage) queryString = queryString.concat(`per_page=${data.articlePerPage}&`);
-    if (data.currentPage) queryString = queryString.concat(`page=${data.currentPage}&`);
-    if (data.writer) queryString = queryString.concat(`writer=${data.writer}&`);
-    if (data.category && data.category !== null)
-        queryString = queryString.concat(`isContestWork=${data.category}&`);
-    if (data.clubs && data.clubs.length > 0)
-        queryString = queryString.concat(`clubs=${JSON.stringify(data.clubs)}&`);
-    if (data.kinds && data.kinds.length > 0)
-        queryString = queryString.concat(`kinds=${JSON.stringify(data.kinds)}`);
-    return queryString;
+    try {
+        let queryString = ``;
+        if (data.perPage) queryString = queryString.concat(`per_page=${data.perPage}&`);
+        if (data.currentPage) queryString = queryString.concat(`page=${data.currentPage}&`);
+        if (data.writer) queryString = queryString.concat(`writer=${data.writer}&`);
+        if (data.category && data.category !== null)
+            queryString = queryString.concat(`isContestWork=${data.category}&`);
+        if (data.clubs && data.clubs.length > 0)
+            queryString = queryString.concat(`clubs=${JSON.stringify(data.clubs)}&`);
+        if (data.kinds && data.kinds.length > 0)
+            queryString = queryString.concat(`kinds=${JSON.stringify(data.kinds)}`);
+        return queryString;
+    } catch (error) {
+        console.log(error);
+    }
 };
