abf4959a54c236c7c2bcbcc60a0354ff1e996071
GangbacOl <gangyeonjo336@gmail.com>
2021-01-27 02:11:43 +0900

    UPDATE TO ADD USER_VALIDATION_API_URL in src/config/config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 export const USER_REGISTER_API_URL = 'https://api.sunrinsecurity.com/user/register';
 export const USER_LOGIN_API_URL = 'https://api.sunrinsecurity.com/user/auth/token';
+export const USER_VALIDATION_API_URL = 'https://api.sunrinsecurity.com/user/auth/validate';
 
 export const ARTICLE_API_URL = 'https://api.sunrinsecurity.com/article';
 
