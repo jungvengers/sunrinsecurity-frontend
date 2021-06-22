2a82cd9af4c02b118b5efca2628dfca88d90714e
GangbacOl <gangyeonjo336@gmail.com>
2021-06-23 01:04:30 +0900

    feat: Update to add uploadThumbnail function

--- src/utils/api/form.js
+++ src/utils/api/form.js
@@ -18,5 +18,13 @@ export const uploadImages = async (data) => {
         })
     );
 };
+
+export const uploadThumbnail = async (data) => {
+    if (!data) return '';
+    const formData = new FormData();
+    formData.append('attachment', data);
+    const response = await axios.post(MEDIA_API_URL, formData, headers);
+    return response.data.filename;
+};
 export const uploadArticle = async (data) => await axios.post(ARTICLE_API_URL, data, headers);
 export const uploadNotice = async (data) => await axios.post(NOTICEBOARD_API_URL, data, headers);
