63f1ee16a2d60d8685dd34f3ec70bad1f67624ff
GangbacOl <gangyeonjo336@gmail.com>
2021-09-30 22:59:28 +0900

    fix: Notice 업로드, 업데이트 페이지에서 'loading' 프로퍼티를 undefined로 인식하는 버그 픽스

--- src/components/Form/Edit/EditNotice.js
+++ src/components/Form/Edit/EditNotice.js
@@ -1,125 +1,141 @@
-import React, { useEffect } from 'react';
-import { Form, Upload, message } from 'antd';
+import React, { useEffect } from "react";
+import { Form, Upload, message } from "antd";
 
-import { InboxOutlined, CloudServerOutlined, DeleteOutlined } from '@ant-design/icons';
+import {
+  InboxOutlined,
+  CloudServerOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
 
-import Header from '../../Header/Header';
-import NoticeSubmit from '../Submit/NoticeSubmit';
-import TextField from '../Input/TextField';
-import Content from '../Input/Content';
-import history from '../../../utils/lib/history';
-import validationToken from '../../../utils/lib/validationToken';
+import Header from "../../Header/Header";
+import NoticeSubmit from "../Submit/NoticeSubmit";
+import TextField from "../Input/TextField";
+import Content from "../Input/Content";
+import history from "../../../utils/lib/history";
+import validationToken from "../../../utils/lib/validationToken";
 
-import './EditArticle.scss';
-import { InfoCircleOutlined } from '@ant-design/icons';
+import "./EditArticle.scss";
+import { InfoCircleOutlined } from "@ant-design/icons";
 
 const EditArticle = ({
-    match,
+  match,
+  id,
+  uploadedFiles,
+  files,
+  youtubeURLs,
+  title,
+  content,
+  updateNoticeStatus,
+  readAnNotice,
+  updateNotice,
+  addImageFile,
+  removeImageFile,
+  removeUploadedImageFile,
+  addYoutubeURL,
+  changeTitle,
+  changeContent,
+}) => {
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) history.push("/");
+    validationToken();
+    readAnNotice(match.params.id);
+  }, [readAnNotice, match.params.id]);
+  const props = {
+    fileList: files,
+    beforeUpload: (file) => {
+      // 파일 사이즈 체킹
+      if (file.size / (1000 * 1000) >= 10) {
+        message.error(
+          `${file.name} 업로드 실패(첨부파일은 10MB까지 허용됩니다.)`
+        );
+        return false;
+      }
+      addImageFile(file);
+      return false;
+    },
+    onRemove: (file) => {
+      removeImageFile(file);
+    },
+  };
+  const submitData = {
     id,
-    uploadedFiles,
     files,
-    youtubeURLs,
+    fileNames: uploadedFiles,
     title,
     content,
-    readAnNotice,
-    updateNotice,
-    addImageFile,
-    removeImageFile,
-    removeUploadedImageFile,
-    addYoutubeURL,
-    changeTitle,
-    changeContent,
-}) => {
-    useEffect(() => {
-        if (!localStorage.getItem('accessToken')) history.push('/');
-        validationToken();
-        readAnNotice(match.params.id);
-    }, [readAnNotice, match.params.id]);
-    const props = {
-        fileList: files,
-        beforeUpload: (file) => {
-            // 파일 사이즈 체킹
-            if (file.size / (1000 * 1000) >= 10) {
-                message.error(`${file.name} 업로드 실패(첨부파일은 10MB까지 허용됩니다.)`);
-                return false;
-            }
-            addImageFile(file);
-            return false;
-        },
-        onRemove: (file) => {
-            removeImageFile(file);
-        },
-    };
-    const submitData = {
-        id,
-        files,
-        fileNames: uploadedFiles,
-        title,
-        content,
-    };
-    return (
-        <div className="EditArticle">
-            <Header />
-            <div>
-                <Form layout="vertical" className="Input">
-                    <Form.Item label="제목">
-                        <TextField
-                            state={title}
-                            setState={changeTitle}
-                            kind="title"
-                            placeholder="제목을 입력해주세요."
-                        />
-                    </Form.Item>
-                    <Form.Item label="내용">
-                        <Content content={content} changeContent={changeContent} />
-                    </Form.Item>
-                    <Form.Item label="업로드된 영상 링크 및 파일">
-                        {uploadedFiles.map((filename) => (
-                            <div className="EditArticle-uploaded-file">
-                                <div>
-                                    <CloudServerOutlined style={{ marginRight: '8px' }} />
-                                    <span>{filename}</span>
-                                </div>
-                                <DeleteOutlined
-                                    className="EditArticle-del-icon"
-                                    onClick={() => removeUploadedImageFile(filename)}
-                                />
-                            </div>
-                        ))}
-                    </Form.Item>
-                    <Form.Item label="파일">
-                        <Upload.Dragger
-                            {...props}
-                            accept="image/png, image/jpg, image/jpeg, .pdf, .zip"
-                            multiple>
-                            <p className="ant-upload-drag-icon">
-                                <InboxOutlined />
-                            </p>
-                            <p className="ant-upload-text">클릭하거나 파일을 여기로 드래그하세요.</p>
-                            <p className="ant-upload-hint">
-                                첨부 형식은 jpg, jpeg, png, zip, pdf이며, 첨부 용량은 개당 최대 10MB까지
-                                허용합니다.
-                            </p>
-                        </Upload.Dragger>
-                    </Form.Item>
-                    <Form.Item
-                        label="영상"
-                        tooltip={{
-                            title: '유튜브 영상의 고유번호를 입력해주세요.',
-                            icon: <InfoCircleOutlined />,
-                        }}>
-                        <TextField
-                            state={youtubeURLs}
-                            setState={addYoutubeURL}
-                            kind="video"
-                            placeholder="링크1 링크2 링크3"
-                        />
-                    </Form.Item>
-                </Form>
-                <NoticeSubmit submitData={submitData} youtubeURLs={youtubeURLs} fetchArticle={updateNotice} />
-            </div>
-        </div>
-    );
+  };
+  return (
+    <div className="EditArticle">
+      <Header />
+      <div>
+        <Form layout="vertical" className="Input">
+          <Form.Item label="제목">
+            <TextField
+              state={title}
+              setState={changeTitle}
+              kind="title"
+              placeholder="제목을 입력해주세요."
+            />
+          </Form.Item>
+          <Form.Item label="내용">
+            <Content content={content} changeContent={changeContent} />
+          </Form.Item>
+          <Form.Item label="업로드된 영상 링크 및 파일">
+            {uploadedFiles.map((filename) => (
+              <div className="EditArticle-uploaded-file">
+                <div>
+                  <CloudServerOutlined style={{ marginRight: "8px" }} />
+                  <span>{filename}</span>
+                </div>
+                <DeleteOutlined
+                  className="EditArticle-del-icon"
+                  onClick={() => removeUploadedImageFile(filename)}
+                />
+              </div>
+            ))}
+          </Form.Item>
+          <Form.Item label="파일">
+            <Upload.Dragger
+              {...props}
+              accept="image/png, image/jpg, image/jpeg, .pdf, .zip"
+              multiple
+            >
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                클릭하거나 파일을 여기로 드래그하세요.
+              </p>
+              <p className="ant-upload-hint">
+                첨부 형식은 jpg, jpeg, png, zip, pdf이며, 첨부 용량은 개당 최대
+                10MB까지 허용합니다.
+              </p>
+            </Upload.Dragger>
+          </Form.Item>
+          <Form.Item
+            label="영상"
+            tooltip={{
+              title: "유튜브 영상의 고유번호를 입력해주세요.",
+              icon: <InfoCircleOutlined />,
+            }}
+          >
+            <TextField
+              state={youtubeURLs}
+              setState={addYoutubeURL}
+              kind="video"
+              placeholder="링크1 링크2 링크3"
+            />
+          </Form.Item>
+        </Form>
+        <NoticeSubmit
+          submitData={submitData}
+          youtubeURLs={youtubeURLs}
+          fetchStatus={updateNoticeStatus}
+          fetchArticle={updateNotice}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default EditArticle;
